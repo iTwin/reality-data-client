caf8e6d34a082d9e0a94e8f3f59e657da15e8b99
Marc-Andre Cote <Marc-Andre.Cote@bentley.com>
2021-11-03 15:45:57 -0400

    fixed linter error

--- src/test/integration/RealityDataClient.test.ts
+++ src/test/integration/RealityDataClient.test.ts
@@ -26,7 +26,7 @@ describe("RealityServicesClient Normal (#integration)", () => {
   // const realityDataServiceClient: RealityDataAccessClient = new RealityDataAccessClient();
   // const imsClient: ImsAuthorizationClient = new ImsAuthorizationClient();
 
-  let projectId: GuidString;
+  let globalProjectId: GuidString;
 
   const tilesId: string = "593eff78-b757-4c07-84b2-a8fe31c19927";
   // const tilesIdWithRootDocPath: string = "3317b4a0-0086-4f16-a979-6ceb496d785e";
@@ -35,8 +35,8 @@ describe("RealityServicesClient Normal (#integration)", () => {
 
   before(async () => {
     accessToken = await TestConfig.getAccessToken();
-    projectId = (await TestConfig.getProjectByName(accessToken, TestConfig.projectName)).id;
-    chai.assert.isDefined(projectId);
+    globalProjectId = (await TestConfig.getProjectByName(accessToken, TestConfig.projectName)).id;
+    chai.assert.isDefined(globalProjectId);
   });
 
   it("should return a RealityData URL properly from a given ID", async () => {
@@ -64,7 +64,7 @@ describe("RealityServicesClient Normal (#integration)", () => {
   it("should return a RealityData from a given ID", async () => {
     try {
       const realityDataAccessClient = new RealityDataAccessClient();
-      const realityData = await realityDataAccessClient.getRealityData(accessToken, projectId, tilesId);
+      const realityData = await realityDataAccessClient.getRealityData(accessToken, globalProjectId, tilesId);
       chai.assert(realityData);
       chai.assert(realityData.id === tilesId);
 
@@ -77,7 +77,7 @@ describe("RealityServicesClient Normal (#integration)", () => {
 
   it("should be able to retrieve the azure blob url", async () => {
     const realityDataAccessClient = new RealityDataAccessClient();
-    const realityData = await realityDataAccessClient.getRealityData(accessToken, projectId, tilesId);
+    const realityData = await realityDataAccessClient.getRealityData(accessToken, globalProjectId, tilesId);
     const url: URL = await realityData.getBlobUrl(accessToken, "test");
     chai.assert(url);
     chai.assert(url.toString().includes("test"));
