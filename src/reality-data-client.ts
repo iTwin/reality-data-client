0788ab89d974e396c20dbcfde4032a6f6c5db615
Marc-Andre Cote <Marc-Andre.Cote@bentley.com>
2021-11-16 14:12:16 -0500

    added missing export

--- src/reality-data-client.ts
+++ src/reality-data-client.ts
@@ -3,6 +3,7 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 export * from "./RealityDataClient";
+export * from "./RealityData";
 
 /** @docs-package-description
  * The reality-data-client package enables integration with reality data stored in ProjectWise Context Share (Reality Data Service).
