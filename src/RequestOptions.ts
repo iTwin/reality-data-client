af0e609a67a67fadf54cd5abb8c80438c3eefe56
Marc-Andre Cote <Marc-Andre.Cote@bentley.com>
2021-11-16 11:29:39 -0500

    implemented preferred representation

--- src/RequestOptions.ts
+++ src/RequestOptions.ts
@@ -9,14 +9,15 @@ import { RequestOptions } from "@bentley/itwin-client";
  * Build the request methods, headers, and other options
  * @param accessTokenString The client access token string
  */
-export function getRequestOptions(accessTokenString: string): RequestOptions {
-  return {
+export function getRequestOptions(accessTokenString: string, returnFullRepresentation: boolean = false): RequestOptions {
+  return{
     method: "GET",
     headers: {
       "authorization": accessTokenString,
       "content-type": "application/json",
       "user-agent": "RealityData Client (iTwinjs) v0.1.0", // TODO figure out how to include build number
       "accept": "application/vnd.bentley.v1+json",
+      "prefer": returnFullRepresentation === true ? "return=representation" : "",
     },
   };
 }
