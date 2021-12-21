956f24b60a1628a76afe519df50b97224eca6fe7
Marc-Andre Cote <Marc-Andre.Cote@bentley.com>
2021-12-21 15:40:35 -0500

    disable user agent header

--- src/RequestOptions.ts
+++ src/RequestOptions.ts
@@ -16,7 +16,7 @@ export function getRequestOptions(accessTokenString: string, url: string, apiVer
     headers: {
       "authorization": accessTokenString,
       "content-type": "application/json",
-      "user-agent": `RealityData Client (iTwinjs) v${process.env.npm_package_version}`,
+      // "user-agent": `RealityData Client (iTwinjs) v${process.env.npm_package_version}`, // TODO eventually put this information in another header, as it causes issues
       "accept": getApiVersionHeader(apiVersion),
       "prefer": returnFullRepresentation === true ? "return=representation" : "",
     },
